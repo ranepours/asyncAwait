dab6d32e2a7c98cc29d7625c57917aa51fc7d7ae
Rane Jasia <ranedavis@ranepours.attlocal.net>
2022-08-30 19:37:51 -0400

    seamless refractoring if i do say so myself

--- cards/app.js
+++ cards/app.js
@@ -31,38 +31,39 @@ function get(url){
 
 let baseURL = "https://deckofcardsapi.com/api/deck";
 
-// //get A card and log its value
-let getCard = get(`${baseURL}/new/draw/`).then(res => {
+//get A card and log its value
+async function getCard(){
+    let res = await get(`${baseURL}/new/draw/`);
     let value = res.data.cards[0].value;
     let suit = res.data.cards[0].suit;
     //console.log(res.data.cards[0]);
     console.log(`${value} of ${suit}`);
-})
+}
+getCard();
 
-// //make two requests FROM THE SAME DECK so we get two separate cards and log their values
-let cardOne;
-let getCards = get(`${baseURL}/new/draw/`).then(res => {
+//make two requests FROM THE SAME DECK so we get two separate cards and log their values
+async function getCards(){
+    let res = await get(`${baseURL}/new/draw/`);
     cardOne = res.data.cards[0];
-    let deckID = res.data.deck_id;
-    return get(`${baseURL}/${deckID}/draw/`)
-}).then(res => {
-    let cardTwo = res.data.cards[0];
+    let deckId = res.data.deck_id;
+    //console.log(deckId);
+    let res2 = await get(`${baseURL}/${deckId}/draw/`);
+    cardTwo = res2.data.cards[0];
     [cardOne, cardTwo].forEach((card) => {
-        console.log(`${card.value} of ${card.suit}`)
+        console.log(`${card.value} of ${card.suit}`);
     })
-})
+}
+getCards();
 
 //build html page which lets u draw from deck and render card to page each time u click "get card" button until all 52 cards have loaded onto page
-let deckID;
-const BTN = document.querySelector("button");
-const cardSpace = document.querySelector("#card-space");
+async function deck(){
+    const BTN = document.querySelector("button");
+    const cardSpace = document.querySelector("#card-space");
 
-let deck = get(`${baseURL}/new/shuffle`).then(res => {
-    return deckID = res.data.deck_id;
-})
-BTN.addEventListener("click", () => {
-    render = get(`${baseURL}/${deckID}/draw/`).then(res => {
-        imgSrc = res.data.cards[0].image;
+    let res = await get(`${baseURL}/new/shuffle/`);
+    BTN.addEventListener("click", async function(){
+        let cardStuff = await get(`${baseURL}/${res.data.deck_id}/draw/`);
+        let imgSrc = cardStuff.data.cards[0].image;
         let card = document.createElement("img");
         card.src = imgSrc;
         console.log(card.src)
@@ -72,6 +73,5 @@ BTN.addEventListener("click", () => {
             alert("All cards pulled!");
         }
     })
-})
-
-//adding this just to change the commit message bc its showing up weird and bothering me
+}
+deck();
